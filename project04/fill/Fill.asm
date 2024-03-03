05223aa4f9fb38e61f85e9423953196df46ecb8d
Borin Ouch <borin.ouch@gmail.com>
2024-03-03 00:08:07 -0800

    Implement Fill.asm

--- project04/fill/Fill.asm
+++ project04/fill/Fill.asm
@@ -3,9 +3,61 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/4/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, 
+// i.e. writes "black" in every pixel. When no key is pressed,
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(LOOP)
+
+  // if key pressed: goto BLK
+  @KBD
+  D=M
+  @BLK
+  D;JNE
+
+  // set fill to white
+  @fill
+  M=0
+
+  // goto DRAW
+  @DRAW
+  0;JMP
+
+  // set fill to black
+  (BLK)
+    @fill
+    M=-1
+
+  (DRAW)
+    @SCREEN
+    D=A
+    @idx
+    M=D
+    @8192
+    D=D+A
+    @end
+    M=D
+
+    (DRAWLOOP)
+      @fill
+      D=M
+      @idx
+      A=M
+      M=D
+      @idx
+      D=M+1
+      M=D
+
+      @end
+      D=D-M
+
+      @DRAWLOOP
+      D;JNE
+
+  @LOOP
+  0;JMP
+
+(END)
+@END
+0;JMP
