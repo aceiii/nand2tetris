d401b0f3117cb21e8580e549481c343f25667a78
Borin Ouch <borin.ouch@gmail.com>
2024-03-20 01:21:42 -0700

    Allow wrting to/reading from stdout/stdin

--- project06/assembler-cpp/src/main.cpp
+++ project06/assembler-cpp/src/main.cpp
@@ -1,4 +1,5 @@
 #include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 #include <argparse/argparse.hpp>
 #include <tl/expected.hpp>
 #include <filesystem>
@@ -6,39 +7,32 @@
 
 #include "assembler.h"
 
-tl::expected<bool, std::string> write_asm_to_file(const std::string& filename, const buffer& buf, bool binary) {
-    auto flags = std::ios::out;
+tl::expected<bool, std::string> write_asm_to_file(std::ostream& out, const buffer& buf, bool binary) {
+    if (!out) {
+        return tl::unexpected(std::strerror(errno));
+    }
     if (binary) {
-        flags |= std::ios::binary;
-    }
-    std::ofstream out(filename, flags);
-    if (out) {
-        if (binary) {
-            for (const auto &byte : buf) {
-                out.write(reinterpret_cast<const char*>(&byte), sizeof(byte));
-            }
-        } else {
-            for (const auto &byte : buf) {
-                std::string line = fmt::format("{:016b}\n", byte);
-                out.write(line.c_str(), line.size());
-            }
+        for (const auto &byte : buf) {
+            out.write(reinterpret_cast<const char*>(&byte), sizeof(byte));
         }
-        out.close();
-        return true;
+    } else {
+        for (const auto &byte : buf) {
+            std::string line = fmt::format("{:016b}\n", byte);
+            out.write(line.c_str(), line.size());
+        }
     }
-    return tl::unexpected(std::strerror(errno));
+    return true;
 }
 
-tl::expected<std::string, std::string> get_file_contents(const std::string& filename) {
-    std::ifstream in(filename, std::ios::in | std::ios::binary);
-    if (in)
-    {
-        std::ostringstream contents;
-        contents << in.rdbuf();
-        in.close();
-        return(contents.str());
+tl::expected<std::string, std::string> get_file_contents(std::istream& in) {
+    // std::ifstream in(filename, std::ios::in);
+    if (!in) {
+        return tl::unexpected(std::strerror(errno));
     }
-    return tl::unexpected(std::strerror(errno));
+
+    std::ostringstream contents;
+    contents << in.rdbuf();
+    return(contents.str());
 }
 
 std::string replace_ext(const std::string& filename, const std::string& ext) {
@@ -71,7 +65,8 @@ tl::expected<void, std::string> set_logging_level(const std::string& level) {
 }
 
 int main(int argc, char* argv[]) {
-    spdlog::set_level(spdlog::level::info);
+    auto logger = spdlog::stderr_color_mt("stderr");
+    spdlog::set_default_logger(logger);
 
     argparse::ArgumentParser program("assembler-cpp", "0.0.1");
 
@@ -92,35 +87,68 @@ int main(int argc, char* argv[]) {
         .default_value(false)
         .implicit_value(true);
 
+    program.add_argument("--stdin")
+        .help("Read input from stdin")
+        .default_value(false)
+        .implicit_value(true);
+
+    program.add_argument("--stdout")
+        .help("Output to stdout instead of file")
+        .default_value(false)
+        .implicit_value(true);
+
     program.add_argument("filename")
         .help("File to assemble.")
+        .default_value("")
         .metavar("FILENAME")
         .nargs(1);
+
+    auto args_error = [&] (const std::string& message) {
+        std::cerr << message << std::endl;
+        std::cerr << program;
+        return 1;
+    };
 
     try {
         program.parse_args(argc, argv);
     } catch(const std::exception& err) {
-        std::cerr << err.what() << std::endl;
-        std::cerr << program;
-        return 1;
+        return args_error(err.what());
     }
 
     const std::string level = program.get("--log-level");
     if (auto result = set_logging_level(level); !result.has_value()) {
-        std::cerr << result.error() << std::endl;
-        std::cerr << program;
-        return 1;
+        return args_error(result.error());
     }
 
     std::string filename = program.get("filename");
     std::string output = program.get("--output");
+    bool read_from_stdin = program.get<bool>("--stdin");
+    bool write_to_stdout = program.get<bool>("--stdout");
 
-    if (output.empty()) {
-        output = replace_ext(std::filesystem::path(filename).filename(), "hack");
+    if (write_to_stdout && !output.empty()) {
+        return args_error("May only use ONE OF --stdin or --output");
     }
 
-    spdlog::info("Reading file: {}", filename);
-    const auto contents = get_file_contents(filename);
+    if ((read_from_stdin && !filename.empty()) || (filename.empty() && !read_from_stdin)) {
+        return args_error("Must read from ONE of FILENAME or --stdin");
+    }
+
+    if (output.empty() && !read_from_stdin) {
+        output = replace_ext(std::filesystem::path(filename).filename(), "hack");
+    } else {
+        output = "out.hack";
+    }
+
+    const auto contents = ([&] () {
+        if (read_from_stdin) {
+            spdlog::info("Reading from STDIN");
+            return get_file_contents(std::cin);
+        }
+
+        spdlog::info("Reading file: {}", filename);
+        std::ifstream file(filename, std::ios::in);
+        return get_file_contents(file);
+    })();
 
     if (!contents.has_value()) {
         spdlog::error("Failed to load file: {}", contents.error());
@@ -134,9 +162,29 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    spdlog::info("Writing to file: {}", output);
-    bool write_binary = program.get<bool>("--binary");
-    if (auto write_result = write_asm_to_file(output, result.value(), write_binary); !write_result.has_value()) {
+
+
+    auto write_result = ([&] () {
+        bool write_binary = program.get<bool>("--binary");
+
+        if (write_to_stdout) {
+            spdlog::info("Writing to STDOUT");
+            return write_asm_to_file(std::cout, result.value(), write_binary);
+        }
+
+        spdlog::info("Writing to file: {}", output);
+
+        auto flags = std::ios::out;
+        if (write_binary) {
+            flags |= std::ios::binary;
+        }
+
+        std::ofstream file(output, flags);
+        return write_asm_to_file(file, result.value(), write_binary);
+        file.close();
+    })();
+
+    if (!write_result.has_value()) {
         spdlog::error("Failed to write to file: {}", write_result.error());
     }
 
